8c11503e90521e6d782dea4a70c34ac9029b295f
xavier <xavier@ocito.com>
2018-04-18 16:50:42 +0200

    added decorator to app implementation

--- app/src/main/java/com/ocito/slantedlayoutapp/MainActivity.kt
+++ app/src/main/java/com/ocito/slantedlayoutapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.ocito.slantedlayoutapp
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration(){
+            override fun getItemOffsets(outRect: Rect?, view: View, parent: RecyclerView?, state: RecyclerView.State?) {
+                if(parent?.getChildAdapterPosition(view) != 0)
+                    outRect?.top = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,-38.0f, resources.displayMetrics).toInt()
+            }
+        })
         val items = ArrayList<Item>()
         for (i in 0..49) {
             items.add(Item())
