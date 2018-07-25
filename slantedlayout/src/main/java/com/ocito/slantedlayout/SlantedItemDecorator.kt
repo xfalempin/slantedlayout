5e7c9915ce892fdd118e0e2ee26f10bcc05ea3f8
xavier <xavier@ocito.com>
2018-07-25 16:52:50 +0200

    changed SlantedItemDecorator to work with pixel value instead of dip to allow easier integration with angle

--- slantedlayout/src/main/java/com/ocito/slantedlayout/SlantedItemDecorator.kt
+++ slantedlayout/src/main/java/com/ocito/slantedlayout/SlantedItemDecorator.kt
@@ -3,15 +3,18 @@ package com.ocito.slantedlayout
 import android.graphics.Rect
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.TypedValue
 import android.view.View
+import kotlin.math.roundToInt
 
 
+/**
+ * slantSize and margin must be in pixels
+ */
 fun RecyclerView.addSlantItemDecoration(@RecyclerView.Orientation orientation:  Int, slantSize: Float, margin: Float) {
     addItemDecoration(object : RecyclerView.ItemDecoration(){
             override fun getItemOffsets(outRect: Rect?, view: View, parent: RecyclerView?, state: RecyclerView.State?) {
                 if(parent?.getChildAdapterPosition(view) != 0) {
-                    val px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, margin - slantSize, resources.displayMetrics).toInt()
+                    val px = (margin - slantSize).roundToInt()
                     when(orientation)
                     {
                         LinearLayoutManager.VERTICAL -> outRect?.top = px
